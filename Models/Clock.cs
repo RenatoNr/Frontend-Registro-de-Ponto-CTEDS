6285ebd89332c8e941bba808456f95d94d5d7c54
Thiago Donas <thidonas@gmail.com>
2022-10-18 18:55:51 -0300

    relatorio com nome e cpf

--- Models/Clock.cs
+++ Models/Clock.cs
@@ -10,6 +10,8 @@ namespace Frontend_Registro_de_Ponto_CTEDS.Models
     {
         public int Id { get; set; }
         public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
         public DateTime? ClockIn { get; set; }
         public DateTime? ClockOut { get; set; }
         public DateTime? LunchIn { get; set; }
--- Relatorio.xaml.cs
+++ Relatorio.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using Frontend_Registro_de_Ponto_CTEDS.Models;
 using Frontend_Registro_de_Ponto_CTEDS.Services;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,14 +26,39 @@ namespace Frontend_Registro_de_Ponto_CTEDS
     public partial class Relatorio : Window
     {
         HttpClient client = new HttpClient();
+        private string uri = "https://localhost:7222";
+        private List<User> Employees { get; set; } = new List<User>();
         public Relatorio()
         {
             InitializeComponent();
+            GetUsers();
             client.BaseAddress = new Uri("https://localhost:7222");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             this.Loaded += Relatorio_Loaded;
         }
+        private async void GetUsers()
+        {
+            using (HttpClient api = new HttpClient())
+            {
+                api.BaseAddress = new Uri(uri);
+                api.DefaultRequestHeaders.Add("Accept", "application/json");
 
+                var getEmployees = await api.GetAsync("/api/Employee");
+
+                var response = await getEmployees.Content.ReadAsStringAsync();
+
+                var jsonObject = JsonConvert.DeserializeObject<List<User>>(response);
+
+                foreach (var item in jsonObject)
+                {
+                    Employees.Add(item);
+                }
+
+
+            }
+
+
+        }
         async void Relatorio_Loaded(object sender, RoutedEventArgs e)
         {
             try
@@ -39,6 +66,19 @@ namespace Frontend_Registro_de_Ponto_CTEDS
 
                 HttpResponseMessage response = await client.GetAsync("/api/Clock");
                 var clocks = await response.Content.ReadAsAsync<IEnumerable<Clock>>();
+                foreach(var clock in clocks)
+                {
+                    var users = Employees.Where(x => x.Id == clock.EmployeeId);
+                    var user = users.ToList();
+                    foreach (var item in user)
+                    {
+                        if(clock.EmployeeId == item.Id)
+                        {
+                            clock.Name = item.Name;
+                            clock.Cpf = item.Cpf;
+                        }
+                    }
+                }
                 reportClock.ItemsSource = clocks;
 
             }
